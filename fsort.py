f3e3d3ac6ef1b7723a3482cc31e0763c8d61f619
dyedfox <slavko7@gmail.com>
2023-01-23 21:47:26 +0200

    Extension handling improved, added README.md

--- fsort.py
+++ fsort.py
@@ -7,19 +7,22 @@ dir = os.getcwd() #get current dir
 dir_list = os.listdir(dir) # Receiving the list of files in current directory
 #dir_list = [f for f in dir_list if os.path.isfile(dir+'/'+f)]
 
-jpegs=[] #For jpeg files only
+files_list=[] #Files list
 
-mask = input('Input end mask (-x):')
+extension = input('Input the file extension (you can ignore this option):')
+mask = input('Input the end mask (number of ending symbols or delimiter):')
 
-for item in dir_list:
-    if item.endswith('.jpg') or item.endswith('.jpeg'):
-        jpegs.append(item)
+if extension:
+    for item in dir_list:
+        if item.endswith(extension):
+            files_list.append(item)
+else:
+    files_list=dir_list
 
 #Creating to-be directories list
-
 if mask.isnumeric():
     mask=int(mask)*-1
-    for file in jpegs:
+    for file in files_list:
         name=file.split('.')[0]
         name=name[:mask]
     # Checking if directory exists
@@ -29,8 +32,9 @@ if mask.isnumeric():
         os.makedirs(dir+'/'+name, exist_ok=True)        # A simpler method to check if dir exists
         os.rename(dir+'/'+file, dir+'/'+name+'/'+file)
 else:
-    for file in jpegs:
-        name=file.split('.')[0]
-        name=name.rsplit(mask, 1)
-        os.makedirs(dir+'/'+name[0], exist_ok=True)        # A simpler method to check if dir exists
-        os.rename(dir+'/'+file, dir+'/'+name[0]+'/'+file)
+    for file in files_list:
+        if mask in file:
+            name=file.split('.')[0]
+            name=name.rsplit(mask, 1)
+            os.makedirs(dir+'/'+name[0], exist_ok=True)        # A simpler method to check if dir exists
+            os.rename(dir+'/'+file, dir+'/'+name[0]+'/'+file)

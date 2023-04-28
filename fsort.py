6c62d827b995a2bb937e1fe51b9389a8e799c989
dyedfox <slavko7@gmail.com>
2023-04-28 09:54:46 +0300

    Refactored code.

--- fsort.py
+++ fsort.py
@@ -1,43 +1,37 @@
-#!/usr/bin/python3
-
 import os
 
-dir = os.getcwd() #get current dir
+def get_files_by_extension(extension):
+    current_directory = os.getcwd()
+    files_list = os.listdir(current_directory)
+    if extension:
+        files_list = [f for f in files_list if f.endswith(extension)]
+    else:
+        files_list = [f for f in files_list if os.path.isfile(os.path.join(current_directory, f))]
+    return files_list
 
-dir_list = os.listdir(dir) # Receiving the list of files in current directory
-#dir_list = [f for f in dir_list if os.path.isfile(dir+'/'+f)]
+def create_directories_from_delimiter(files_list, delimiter):
+    for file_name in files_list:
+        if delimiter.isdigit():
+            directory_name = file_name.rsplit('.', 1)[0][:-int(delimiter)]
+        else:
+            directory_name = file_name.rsplit('.', 1)[0].rsplit(delimiter, 1)[0]
+        if directory_name != file_name:  # Prevents creating directory for files without delimiter
+            directory_path = os.path.join(os.getcwd(), directory_name)
+            os.makedirs(directory_path, exist_ok=True)
+            file_path = os.path.join(os.getcwd(), file_name)
+            os.rename(file_path, os.path.join(directory_path, file_name))
 
-files_list=[] #Files list
-print ('''
->> fsort version 0.1.1 -  fsort is a simple tool for distributing files to folders based on their name prefixes and file extensions.
-(https://github.com/dyedfox/fsort)
+def main():
+    print('''
+    >> fsort version 0.3.1 -  fsort is a simple tool for distributing files to folders based on their name prefixes and file extensions.
+    (https://github.com/dyedfox/fsort)
 
-*Please note: a directory is considered a file with null extension. Be careful while using this option!
-''')
-extension = input('>> Please enter the file extension (you can leave this option blank if you want to sort all files regardless of their extension): ')
-mask = input('>> Please enter the number of ending characters or delimiter to use for sorting files: ')
+    *Please note: a directory is considered a file with null extension. Be careful while using this option!
+    ''')
+    extension = input('>> Please enter the file extension (you can leave this option blank if you want to sort all files regardless of their extension): ')
+    delimiter = input('>> Please enter the number of ending characters or delimiter to use for sorting files: ')
+    files_list = get_files_by_extension(extension)
+    create_directories_from_delimiter(files_list, delimiter)
 
-if extension:
-    for item in dir_list:
-        if item.endswith(extension):
-            files_list.append(item)
-else:
-    files_list=dir_list
-
-#Creating to-be directories list
-if mask.isnumeric():
-    mask=int(mask)*-1
-    for file in files_list:
-        name=file.rsplit('.', 1)[0][:mask]
-    # Checking if directory exists
-    #    if not os.path.exists(dir+'/'+name):
-    #        os.makedirs(dir+'/'+name)
-
-        os.makedirs(dir+'/'+name, exist_ok=True)        # A simpler method to check if dir exists
-        os.rename(dir+'/'+file, dir+'/'+name+'/'+file)
-else:
-    for file in files_list:
-        if mask in file:
-            name=file.rsplit('.', 1)[0].rsplit(mask, 1)
-            os.makedirs(dir+'/'+name[0], exist_ok=True)        # A simpler method to check if dir exists
-            os.rename(dir+'/'+file, dir+'/'+name[0]+'/'+file)
+if __name__ == '__main__':
+    main()

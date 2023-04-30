3b6af0f2b2b88776eae726f8ff1f467b75248556
dyedfox <slavko7@gmail.com>
2023-04-30 10:44:54 +0300

    Added possibility to enter ending position

--- yasort.py
+++ yasort.py
@@ -9,12 +9,12 @@ def get_files_by_extension(extension):
         files_list = [f for f in files_list if os.path.isfile(os.path.join(current_directory, f))]
     return files_list
 
-def create_directories_from_delimiter(files_list, delimiter):
+def create_directories_from_delimiter(files_list, delimiter, delimiter_position):
     for file_name in files_list:
         if delimiter.isdigit():
             directory_name = file_name.rsplit('.', 1)[0][:-int(delimiter)]
         else:
-            directory_name = file_name.rsplit('.', 1)[0].rsplit(delimiter, 1)[0]
+            directory_name = file_name.rsplit('.', 1)[0].rsplit(delimiter, int(delimiter_position))[0]
         if directory_name != file_name:  # Prevents creating directory for files without delimiter
             directory_path = os.path.join(os.getcwd(), directory_name)
             os.makedirs(directory_path, exist_ok=True)
@@ -23,15 +23,21 @@ def create_directories_from_delimiter(files_list, delimiter):
 
 def main():
     print('''
-    >> fsort version 0.3.1 -  fsort is a simple tool for distributing files to folders based on their name prefixes and file extensions.
+    >> yasort version 0.3.1 -  yasort (Yet Another Sort Tool) is a simple tool for distributing files to folders based on their name prefixes and file extensions.
     (https://github.com/dyedfox/fsort)
 
     *Please note: a directory is considered a file with null extension. Be careful while using this option!
     ''')
     extension = input('>> Please enter the file extension (you can leave this option blank if you want to sort all files regardless of their extension): ')
     delimiter = input('>> Please enter the number of ending characters or delimiter to use for sorting files: ')
+
+    if delimiter.isdigit():
+        files_list = get_files_by_extension(extension)
+        position=1
+    else:
+        position=input(">> Please enter the number of delimiter's position from the end: ")
     files_list = get_files_by_extension(extension)
-    create_directories_from_delimiter(files_list, delimiter)
+    create_directories_from_delimiter(files_list, delimiter, position)
 
 if __name__ == '__main__':
     main()
